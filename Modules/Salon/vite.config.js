08fd14c385017412e461c78b8057db28eb76fdd7
Phuong <phuong@lessgap.com>
2023-11-06 18:51:44 +0700

    update nginx

--- Modules/Salon/vite.config.js
+++ Modules/Salon/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   configureWebpack: {
     resolve: {
       alias: {
-        vue$: "vue/dist/vue.common"
+        vue$: 'vue/dist/vue.esm-bundler.js',
       }
     }
   },
