c1ac37c77c0b929e44a602df1ac7717a080a155d
Phuong <phuong@lessgap.com>
2023-11-06 21:13:55 +0700

    update nginx

--- Modules/Salon/src/utils/RealtimeDB.js
+++ Modules/Salon/src/utils/RealtimeDB.js
@@ -1,14 +1,14 @@
 import Surreal from "https://cdn.jsdelivr.net/npm/surrealdb.js";
 
 const db = new Surreal();
-db.connect(import.meta.env.VITE_RPC_HOST ? import.meta.env.VITE_RPC_HOST : 'http://localhost:8000/rpc', {
-  ns: 'unify',
-  db: 'unify',
-  auth: {
-    user: 'viewer',
-    pass: '123456',
-  },
-});
+// db.connect(import.meta.env.VITE_RPC_HOST ? import.meta.env.VITE_RPC_HOST : 'http://localhost:8000/rpc', {
+//   ns: 'unify',
+//   db: 'unify',
+//   auth: {
+//     user: 'viewer',
+//     pass: '123456',
+//   },
+// });
 
 
 export {
